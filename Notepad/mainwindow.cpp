e8aee1267e1ddeabac5e11cb495b45a5b2c92f5a
Ravindu Jayathilaka <57854539+Ravindu-Jayathilaka@users.noreply.github.com>
2022-01-16 01:30:29 +0530

    finished

--- Notepad/mainwindow.cpp
+++ Notepad/mainwindow.cpp
@@ -1,15 +1,115 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);//removing the extra space in left and right to the text edit
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_actionNew_triggered()
+{
+    filePath = "";
+    ui->textEdit->setText("");
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,"Open the file");
+    QFile file(fileName);
+    filePath = fileName;
+    if(!file.open(QFile::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this,"..","file not open");
+        return;
+    }
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+
+void MainWindow::on_actionSave_triggered()
+{
+
+    QFile file(filePath);
+    if(!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this,"..","file not open");
+        return;
+    }
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.flush();
+    file.close();
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+  ui->textEdit->copy();
+}
+
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,"Open the file");
+    QFile file(fileName);
+    filePath = fileName;
+    if(!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this,"..","file not open");
+        return;
+    }
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.flush();
+    file.close();
+}
+
+
+void MainWindow::on_actionCut_triggered()
+{
+   ui->textEdit->cut();
+}
+
+
+void MainWindow::on_actionPaste_triggered()
+{
+   ui->textEdit->paste();
+}
+
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QString aboutText;
+    aboutText = "This is a simple notepad application. You can use this to write down things instead of memorising.\n";
+    aboutText += "Created by Ravindu Jayathilaka";
+    QMessageBox::about(this, "",aboutText);
 }
 
--- Notepad/mainwindow.h
+++ Notepad/mainwindow.h
@@ -15,7 +15,29 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionSave_as_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionAbout_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QString filePath;
 };
 #endif // MAINWINDOW_H
